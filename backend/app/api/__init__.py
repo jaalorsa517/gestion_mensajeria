6f1170b02e9056ed219d83620d9f9299dac31a20
Jaime <jaalorsa517@hotmail.com>
2021-01-23 10:00:03 -0500

    Se separo el frontend del backend. Este es el inicio

--- backend/app/api/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask_restful import Api
-from app import app
-
-api = Api(app,'/api/v1')
-
-import app.api.v1
--- backend/app/auth/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask_httpauth import HTTPBasicAuth
-from app.models.users import get_user
-
-auth = HTTPBasicAuth('Basic')
-
-@auth.verify_password
-def verify_password(username,password):
-    if get_user(username,password):
-        return username
